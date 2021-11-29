e0cac1299ff5b5eb734d87b79f8175b63a650bfb
octavio <octaviosuriellopezlopez@gmail.com>
2021-11-29 11:43:25 -0600

    se implemento un nav (solo estructura)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponenteNavComponent } from './componente-nav/componente-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponenteNavComponent
   ],
   imports: [
     BrowserModule,
